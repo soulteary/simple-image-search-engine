15ebc2df90c55978e1b12a6b87885fb32ff777d3
soulteary <soulteary@gmail.com>
2023-11-14 22:59:36 +0800

    chore: use redis-server instead of ip addr

--- steps/4.save-embededing-to-redis/app.py
+++ steps/4.save-embededing-to-redis/app.py
@@ -6,7 +6,7 @@ import time
 import redis
 
 # 连接 Redis 数据库，地址换成你自己的 Redis 地址
-client = redis.Redis(host="10.11.12.178", port=6379, decode_responses=True)
+client = redis.Redis(host="redis-server", port=6379, decode_responses=True)
 res = client.ping()
 print("redis connected:", res)
 
